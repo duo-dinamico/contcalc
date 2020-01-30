e39421d2d3b3be9a20eee5b0536bcbf042f8249a
Joao Jesus <joaodejesus@gmail.com>
2020-01-30 22:57:39 +0000

    added label and entry into page1

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jan 30 19:33:30 2020
-
-@author: sj
+# odetojoy
+@author: The JJ duo
 """
 
 from tkinter import ttk
@@ -20,6 +20,10 @@ def main():
     # adding Frames as pages for the ttk.Notebook 
     # first page, which would get widgets gridded into it
     page1 = ttk.Frame(nb)
+    L1 = Label(page1, text="Job Title") .grid(row=0, column=0)
+    # L1.pack(expand=1, fill="both")
+    E1 = Entry(page1, bd =5) .grid(row=0, column=1)
+    # E1.pack(expand=1, fill="both")
 
     # second page
     page2 = ttk.Frame(nb)
