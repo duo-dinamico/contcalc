f82419b8a760ef600a5ba2683dae0eccad2739b1
Joao Jesus <joaodejesus@gmail.com>
2020-02-02 12:12:14 +0000

    tabela de lookup feita com dicionarios para testar a ordem das coisas quando aparece na janela principal. dicionario sera substituido por ligacao a sqlite mais tarde?

--- pageLookup.py
+++ pageLookup.py
@@ -7,5 +7,26 @@ pageLookup.py v0.1
 Copyright 2020, The JJ duo
 """
 
-def pageL():
-    return 'Lookup'
+def pageL_Tray():
+    d1 = {
+    'Tray Sizes': ['900','750','600','450','300','225','150','100','75','50']
+    }
+
+    # title = str(d1.keys())
+    # content = str(d1.values())
+
+    for k, v in d1.items():
+        #return ''.join('{}{}'.format(v,'\n'))
+        return k + '\n' + '\n'.join(v)
+
+def pageL_Ladder():
+    d1 = {
+    'Ladder Sizes': ['900','750','600','450','300','225','150','150','150','150']
+    }
+
+    # title = str(d1.keys())
+    # content = str(d1.values())
+
+    for k, v in d1.items():
+        #return ''.join('{}{}'.format(v,'\n'))
+        return k + '\n' + '\n'.join(v)
