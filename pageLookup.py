58c36e4469dddad8951a17571f94f91c395a9c41
Joao Jesus <joaodejesus@gmail.com>
2020-02-02 21:15:49 +0000

    General page setup finished. Text used for testing. Further testing with .grid might be necessary.

--- pageLookup.py
+++ pageLookup.py
@@ -12,9 +12,6 @@ def pageL_Tray():
     'Tray Sizes': ['900','750','600','450','300','225','150','100','75','50']
     }
 
-    # title = str(d1.keys())
-    # content = str(d1.values())
-
     for k, v in d1.items():
         #return ''.join('{}{}'.format(v,'\n'))
         return k + '\n' + '\n'.join(v)
@@ -24,9 +21,16 @@ def pageL_Ladder():
     'Ladder Sizes': ['900','750','600','450','300','225','150','150','150','150']
     }
 
-    # title = str(d1.keys())
-    # content = str(d1.values())
-
     for k, v in d1.items():
         #return ''.join('{}{}'.format(v,'\n'))
+        return k + '\n' + '\n'.join(v)
+
+def pageL_Cables():
+    d1 = {
+    'Cable Type': ['XLPE/SWA/PVC', 'XLPE/SWA/LSF', 'FP400', 'FP600S', 'MICC (Light Duty)', 'MICC (Heavy Duty)', 'LSF Single'],
+    'Cores': ['1', '2', '3', '4', '5', '7', '12', '19', '27', '37'],
+    'CSA': ['1', '1.5', '2.5', '4', '6', '10', '16', '25', '35', '50', '70', '95', '120', '150', '185', '240', '300', '400', '500', '630', '800', '1000']
+    }
+
+    for k, v in d1.items():
         return k + '\n' + '\n'.join(v)
