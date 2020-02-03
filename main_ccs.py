010081f54bea837c0a229f4c4811968125b7ad96
Joao Jesus <joaodejesus@gmail.com>
2020-02-03 20:40:31 +0000

    general page updated

--- main_ccs.py
+++ main_ccs.py
@@ -25,40 +25,51 @@ pageL = pageLookup
 # Initiate the main window for tkinter
 mainWindow = tk.Tk()
 mainWindow.title('Containment Calculation Sheet')
-mainWindow.geometry('500x500')
+#mainWindow.geometry('500x500')
+#mainWindow.rowconfigure(5, {'minsize': 100})
+#mainWindow.columnconfigure(8, {'minsize': 200})
 
 # Initiate the notebook inside the main window
-nb = ttk.Notebook(mainWindow, height=500, width=500)
-nb.grid()
+nb = ttk.Notebook(mainWindow)
+nb.grid(row=0, column=0)
 
 # Page 1 for General information about the project
 # General page might not link to external module
-page1 = ttk.Frame(nb, height=500, width=500)
+page1 = ttk.Frame(nb)
+
+rows = 0
+while rows < 5:
+    page1.rowconfigure(rows, {'minsize': 100}, weight=1)
+    page1.columnconfigure(rows, {'minsize': 100}, weight=1)
+    rows += 1
+
 nb.add(page1, text='General Information')
 # L1 = tk.Label(page1, text=pageG.pageG()) .grid(row=0, column=0)
 gifLogo = tk.PhotoImage(file='ArupLogo.gif')
 #logoCanvas = tk.Canvas(page1)
 #logoCanvas.grid(row=0, column=0)
 #logoCanvas.create_image(50, 20, anchor='center', image=gifLogo)
-logoLabel = tk.Label(page1, image=gifLogo).grid(row=0, columnspan=8, pady=20)
 
-jobTitleLabel1 = tk.Label(page1, text='Job Title').grid(row=1, column=0, columnspan=4, sticky='W', pady=10)
-jobTitleLabel2 = tk.Label(page1, text='Project Churchill', bg='white').grid(row=1, column=4, columnspan=4, sticky='E')
+logoLabel = tk.Label(page1, image=gifLogo).grid(row=0, column=0, columnspan=5, sticky='NSWE')
 
-jobNumberLabel1 = tk.Label(page1, text='Job Number').grid(row=2, column=0, sticky='W', pady=10,)
+jobTitleLabel1 = tk.Label(page1, text='Job Title').grid(row=1, column=0, columnspan=2, sticky='W')
+jobTitleLabel2 = tk.Label(page1, text='Project Churchill', bg='white').grid(row=1, column=3, columnspan=3, sticky='W')
+
+jobNumberLabel1 = tk.Label(page1, text='Job Number').grid(row=2, column=0, sticky='W')
 jobNumberLabel2 = tk.Label(page1, text='200000', bg='white').grid(row=2, column=1, sticky='E')
-refLabel1 = tk.Label(page1, text='Reference').grid(row=2, column=6, sticky='W', pady=10)
-refLabel2 = tk.Label(page1, text='A', bg='white').grid(row=2, column=7, sticky='E')
+refLabel1 = tk.Label(page1, text='Reference').grid(row=2, column=3, sticky='W')
+refLabel2 = tk.Label(page1, text='A', bg='white').grid(row=2, column=4, sticky='E')
 
-dateLabel1 = tk.Label(page1, text='Date').grid(row=3, column=0, sticky='W', pady=10)
+dateLabel1 = tk.Label(page1, text='Date').grid(row=3, column=0, sticky='W')
 dateLabel2 = tk.Label(page1, text='2020/02/02', bg='white').grid(row=3, column=1, sticky='E')
-designerLabel1 = tk.Label(page1, text='Designer').grid(row=4, column=6, sticky='W', pady=10)
-designerLabel2 = tk.Label(page1, text='JJ', bg='white').grid(row=4, column=7, sticky='E')
+designerLabel1 = tk.Label(page1, text='Designer').grid(row=3, column=3, sticky='W')
+designerLabel2 = tk.Label(page1, text='JJ', bg='white').grid(row=3, column=4, sticky='E')
 
-revLabel1 = tk.Label(page1, text='Revision').grid(row=4, column=0, sticky='W', pady=10)
+revLabel1 = tk.Label(page1, text='Revision').grid(row=4, column=0, sticky='W')
 revLabel2 = tk.Label(page1, text='P01', bg='white').grid(row=4, column=1, sticky='E')
 
-titleLabel = tk.Label(page1, text='ESN Calc Sheet\nContainment sizing spreadsheet\nRevision 0.1 Feb20', justify='center').grid(row=5, column=0, columnspan=8, sticky='NSWE')
+titleLabel = tk.Label(page1, text='ESN Calc Sheet\nContainment sizing spreadsheet\nRevision 0.1 Feb20', justify='center').grid(row=5, column=0, columnspan=5, sticky='NSWE')
+
 
 # Page 2 for Calculation information
 page2 = ttk.Frame(nb)
