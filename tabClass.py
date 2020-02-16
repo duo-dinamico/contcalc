66d7d6c4f0ef13c607756ee990289f0d063907a0
jcvsilva <jcvsilva@gmail.com>
2020-02-16 23:35:59 +0000

    tabClass improve. JCS

--- tabClass.py
+++ tabClass.py
@@ -18,8 +18,20 @@ class MyTab(ttk.Frame):
         ttk.Frame.__init__(self, parent)
         self.parent = parent
 
+        # Test Labelframe
+        self.common_parameters = ttk.Labelframe(self, text='Common')
+        self.common_parameters.grid()
+        self.cable_parameters = ttk.Labelframe(self, text='Cables')
+        self.cable_parameters.grid()
+
         self.name = name
+        self.install_type_var = tk.StringVar()
+        self.custom_spacing_var = tk.StringVar()
         self.cable_list = []
+        self.cable_ref_var = tk.StringVar()
+        self.cable_type_var = tk.StringVar()
+        self.cable_cores_var = tk.StringVar()
+
 
         parent.add(self, text=self.name)
         self.tab_draw()
@@ -27,9 +39,39 @@ class MyTab(ttk.Frame):
     def tab_draw(self):
         """Method that draw all widgets in the tab"""
 
+        # Common - Installation type
+        self.install_type_label = tk.Label(self.common_parameters, text='Installation type:')
+        self.install_type_label.grid(row=0, column=0)
+        self.install_type_entry = tk.Entry(self.common_parameters, textvariable=self.install_type_var)
+        self.install_type_entry.grid(row=0, column=1)
+
+        # Common - Custom spacing
+        self.custom_spacing_label = tk.Label(self.common_parameters, text='Custom spacing:')
+        self.custom_spacing_label.grid(row=0, column=2)
+        self.custom_spacing_entry = tk.Entry(self.common_parameters, textvariable=self.custom_spacing_var)
+        self.custom_spacing_entry.grid(row=0, column=3)
+
+        # Entry for Cable ref
+        self.cable_ref_label = tk.Label(self.cable_parameters, text='Ref')
+        self.cable_ref_label.grid(row=0, column=0)
+        self.cable_ref_entry = tk.Entry(self.cable_parameters, textvariable=self.cable_ref_var)
+        self.cable_ref_entry.grid(row=0, column=1)
+
+        # Select cable type
+        self.cable_type_label = tk.Label(self.cable_parameters, text='Type')
+        self.cable_type_label.grid(row=1, column=0)
+        self.cable_type_optionmenu = tk.OptionMenu(self.cable_parameters, self.cable_type_var, *self.get_type_list(), command=self.cable_type_select)
+        self.cable_type_optionmenu.grid(row=1, column=1)
+
+        # Select core Number
+        self.cable_cores_label = tk.Label(self.cable_parameters, text='Cores')
+        self.cable_cores_label.grid(row=2, column=0)
+        self.cable_cores_optionmenu = tk.OptionMenu(self.cable_parameters, self.cable_cores_var, *self.get_cores_list(), command=self.cable_cores_select)
+        self.cable_cores_optionmenu.grid(row=2, column=1)
+
         # Delete_tab button
         self.del_tab_btn = tk.Button(self, text='Delete Section', width=12, command=self.delete_this_tab)
-        self.del_tab_btn.grid()
+        self.del_tab_btn.grid(row=5, column=0)
 
     def delete_this_tab(self):
         """Method to destroy current tab"""
@@ -52,6 +94,25 @@ class MyTab(ttk.Frame):
         for cable in self.cable_list:
             result_list.append(cable.cable_ref)
         return result_list
+
+    def get_type_list(self):
+        """Method that returns the list of all cables abvailable"""
+
+        # Um dia vai ser aqui criada uma lista de referencias de cabos,
+        # mas esse dia não é hoje
+        return ['Cabo A', 'Cabo B', 'Cabo C']
+
+    def get_cores_list(self):
+        """Method that returns the list of all cores abvailable"""
+
+        # Um dia vai ser aqui criada uma lista de cores de cabos,
+        # mas esse dia não é hoje
+        return ['1 core', '2 core', '3 core']
+
+    def cable_type_select(self, event):
+        print(self.cable_type_var.get())
+    def cable_cores_select(self, event):
+        print(self.cable_cores_var.get())
 
 
 
