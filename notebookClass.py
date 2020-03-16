505613883b6810f1e220607dda028f10180b9b0d
mapsy <joaodejesus@gmail.com>
2020-03-16 21:31:57 +0000

    made the status of the Save more consistent.

--- notebookClass.py
+++ notebookClass.py
@@ -47,7 +47,7 @@ class Notebook(ttk.Notebook):
         self.info = CreateLabelframe(frame, text='General Info', row=1, column=0, colspan=4, sticky='NSWE', ipadx=0, ipady=0, padx=20, pady=20)
         self.cSection = CreateLabelframe(frame, text='Create New Section', row=2, column=0, colspan=4, sticky='NSWE', ipadx=0, ipady=0, padx=20, pady=20)
         """Labels with logo"""
-        CreateLabel(frame, text='', image=self.gifLogo, background='white', height=200, width=1020, row=0, column=0, colspan=4, sticky='NSEW')
+        CreateLabel(frame, text='', image=self.gifLogo, background='white', height=200, width=925, row=0, column=0, colspan=4, sticky='NSEW')
 
         """Labels for column 0"""
         CreateLabel(self.info, text='Job Title: ', image='', background=None, height=3, width=0, row=0, column=0, colspan=1, sticky='EW')
